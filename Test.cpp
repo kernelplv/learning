97335094e87e7415f824e857b3cb7139e69af0c6
kernelplv <kernelplv@gmail.com>
2019-04-12 20:54:37 +0300

    Suite class refactor for new style Test  class refactor for new style

--- Test.cpp
+++ Test.cpp
@@ -5,32 +5,45 @@
 using namespace std;
 using namespace TestSuite;
 
-void Test::do_test(bool cond, const std::string& lbl, const char* fname, long lineno) {
-    if (!cond) 
-        do_fail(lbl, fname, lineno);
-    else
-        succeed_();
+void Test::do_test(               
+                    bool   cond , 
+       const std::string&   lbl , 
+              const char* fname , 
+                    long lineno ) 
+{
+  if ( not cond ) 
+      do_fail( lbl, fname, lineno ) ;
+  else
+      succeed_()                    ;
 }
 
-void Test::do_fail(const std::string& lbl, const char* fname, long lineno) {
-    ++nFail;
-    if (osptr) {
-        *osptr << "[ " 
-                << typeid(*this).name() 
-                <<" ]"
-                << endl 
-                << "failure: (" << lbl << ") . "
-                << fname 
-                << " (line " << lineno << ")" 
-                << endl; 
-    }
+void Test::do_fail(
+                    const std::string&   lbl ,
+                           const char* fname , 
+                                 long lineno ) 
+{
+  ++nFail ;
+  if ( osptr ) 
+     {
+       *osptr << "[ " 
+              << typeid(*this).name() 
+              <<" ]"
+              << endl 
+              << "failure: (" << lbl << ") . "
+              << fname 
+              << " (line " << lineno << ")" 
+              << endl ; 
+     }
 }
 
-long Test::report() const {
-    if (osptr) { 
-        *osptr << "Test \"" << typeid(*this).name()
-               << "\":\n\tPassed: " << nPass
-               << "\tFailed: " << nFail << endl;
-    }
+long Test::report() const 
+{
+  if ( osptr ) 
+     { 
+       *osptr << "Test \"" << typeid(*this).name()
+              << "\":\n\tPassed: " << nPass
+              << "\tFailed: " << nFail << endl ;
+     }
+     
     return nFail;
 }
